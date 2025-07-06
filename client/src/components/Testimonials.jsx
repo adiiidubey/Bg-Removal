6547edb0dd5c64b737d2b58191e8aa8db044ccc0
adiidubey <dubeyaditya397@gmail.com>
2025-07-06 09:41:49 +0530

    First Commit

---
+++ client/src/components/Testimonials.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Testimonials = () => {
+  return (
+		<div>
+			{/* Title */}
+			<h1 className="text-center text-2xl md:text-3xl lg:text-4xl mt-4 font-semibold bg-gradient-to-r from-gray-900 to-gray-400 bg-clip-text text-transparent py-5">
+				Customer Testimonials
+			</h1>
+		</div>
+  );
+}
+
+export default Testimonials
